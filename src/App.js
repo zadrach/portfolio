10982c48aa259490c863ca215e95269863e40337
ZadrachCaunca <zadrachcaunca777@gmail.com>
2023-08-31 01:05:23 +0800

    add link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './MenuBar.css'; // Import the menu bar styles
 
 function App() {
   return (
-    <Router basename="/porftolio">
+    <Router basename="/porftolio/skills/projects/contact">
       <div>
         {/* Load Google Font */}
         <GoogleFontLoader fonts={[{ font: 'Ultra', weights: [400, 700] }]} />
